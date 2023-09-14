f57a55e88514c67ec4a6108cd79a9ad30452d2ec
ramirogvalverde <ramirogvalverde@gmail.com>
2023-09-14 10:56:38 +0200

    AD_Tema_1

--- AD/EVALUACION_1/T1_FICHEROS/EJERCICIOS/EJEMPLOS_TEORIA/SYSTEM/src/ficheros/Ficheros.java
+++ AD/EVALUACION_1/T1_FICHEROS/EJERCICIOS/EJEMPLOS_TEORIA/SYSTEM/src/ficheros/Ficheros.java
@@ -13,7 +13,7 @@ public class Ficheros {
     public static void main(String[] args) {
         
         
-        System.out.println("El sistema operativo es: " + System.getProperty("os.name") + " versión: " + System.getProperty("os.version"));
+        System.out.println("El sistema operativo es: " + System.getProperty("os.name") + ", versión: " + System.getProperty("os.version"));
         System.out.println("El directorio del usuario es: " + System.getProperty("user.home"));
         System.out.println("El nombre del usuario: " + System.getProperty("user.name"));
         System.out.println("El directorio de trabajo es: " + System.getProperty("user.dir"));
