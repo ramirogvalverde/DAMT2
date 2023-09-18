df4c578cabd50aac1f6b3aaa9249c03933da32fd
ramirogvalverde <ramirogvalverde@gmail.com>
2023-09-18 14:00:36 +0200

    ContraseniaConRegex

--- PSP/EVALUACION_1/EJERCICIOS_REPASO/Contrasenias/src/contrasenias/ContraseniaSinRegex.java
+++ PSP/EVALUACION_1/EJERCICIOS_REPASO/Contrasenias/src/contrasenias/ContraseniaSinRegex.java
@@ -7,45 +7,55 @@ public class ContraseniaSinRegex {
     public static void main(String[] args) {
 
         String cadena;
+        boolean esCorrecto = false;
         boolean esMinuscula = false;
         boolean esMayuscula = false;
         boolean esNumero = false;
         boolean esEspecial = false;
-        Scanner lectura = new Scanner(System.in);
-        System.out.println("Introduce un password correcto");
 
-        cadena = lectura.next();
+        do {
+            Scanner lectura = new Scanner(System.in);
+            System.out.println("Introduce un password correcto");
 
-        if (cadena.length() < 8) {
-            System.out.println("la longitud del password no es correcta");
+            cadena = lectura.next();
+            if (cadena.length() < 8) {
+                System.out.println("la longitud del password no es correcta");
 
-        } else {
+            } else {
 
-            for (int i = 0; i < cadena.length(); i++) {
-                char caracter = cadena.charAt(i);
+                for (int i = 0; i < cadena.length(); i++) {
+                    char caracter = cadena.charAt(i);
 
-                if (Character.isLowerCase(caracter)) {
-                    esMinuscula = true;
+                    if (Character.isLowerCase(caracter)) {
+                        esMinuscula = true;
+                    }
+                    if (Character.isUpperCase(caracter)) {
+                        esMayuscula = true;
+                    }
+                    if (Character.isDigit(caracter)) {
+                        esNumero = true;
+                    }
+                    if (caracter == '-' || caracter == '_' || caracter == '/') {
+                        esEspecial = true;
+                    }
+
                 }
-                if (Character.isUpperCase(caracter)) {
-                    esMayuscula = true;
-                }
-                if (Character.isDigit(caracter)){
-                    esNumero= true;
-                }
-                if (caracter=='-'||caracter=='_'||caracter=='/'){
-                    esEspecial=true;
-                }
+                if (!esMinuscula){System.out.println("Necesitas al menos una minúscula");};
+                if (!esMayuscula){System.out.println("Necesitas al menos una mayúscula");};
+                if (!esNumero){System.out.println("Necesitas al menos un número");};
+                if (!esEspecial){System.out.println("Necesitas al menos un caracter especial (- _ /)");};
                 
-            }
-            if(esMinuscula && esEspecial && esNumero && esMayuscula){
-                System.out.println("El password es correcto");
-                
-            }else{
-                System.out.println("El password es incorrecto");
+                if (esMinuscula && esEspecial && esNumero && esMayuscula) {
+                    System.out.println("El password es correcto");
+                    esCorrecto = true;
+
+                } else {
+                    System.out.println("El password es incorrecto");
+                }
+
             }
 
-        }
+        } while (!esCorrecto);
 
     }
 
--- PSP/EVALUACION_1/EJERCICIOS_REPASO/Contrasenias/src/contrasenias/ContraseniasConRegex.java
+++ PSP/EVALUACION_1/EJERCICIOS_REPASO/Contrasenias/src/contrasenias/ContraseniasConRegex.java
@@ -1,20 +1,41 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package contrasenias;
 
-/**
- *
- * @author damt210
- */
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class ContraseniasConRegex {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner lectura = new Scanner(System.in);
+            System.out.println("Introduce un password correcto");
+
+            String cadena = lectura.next();
+
+            if(isMatchingRegex(cadena)==true){
+                System.out.println("password correcto");
+            
+            }else{
+                System.out.println("password incorrecto");
+            }
+
+    }
+    private static final Pattern[] inputRegexes = new Pattern[4];
+
+    static {
+        inputRegexes[0] = Pattern.compile(".*[A-Z].*");
+        inputRegexes[1] = Pattern.compile(".*[a-z].*");
+        inputRegexes[2] = Pattern.compile(".*[0-9].*");
+        inputRegexes[3] = Pattern.compile(".*[-_/].*");
     }
     
+    private static boolean isMatchingRegex(String input) {
+    boolean inputMatches = true;
+    for (Pattern inputRegex : inputRegexes) {
+        if (!inputRegex.matcher(input).matches()) {
+            inputMatches = false;
+        }
+    }
+    return inputMatches;
+}
+
 }
