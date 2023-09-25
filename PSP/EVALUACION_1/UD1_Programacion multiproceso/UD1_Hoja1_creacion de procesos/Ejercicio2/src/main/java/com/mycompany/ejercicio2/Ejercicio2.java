0ec5680a29a7ddb09ecd5400b7b2c782563440f0
ramirogvalverde <ramirogvalverde@gmail.com>
2023-09-25 21:12:46 +0200

    25/9 despues recreo

--- PSP/EVALUACION_1/UD1_Programacion multiproceso/UD1_Hoja1_creacion de procesos/Ejercicio2/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
+++ PSP/EVALUACION_1/UD1_Programacion multiproceso/UD1_Hoja1_creacion de procesos/Ejercicio2/src/main/java/com/mycompany/ejercicio2/Ejercicio2.java
@@ -22,26 +22,16 @@ public class Ejercicio2 {
                 Runtime rt = Runtime.getRuntime();
                 String[] comando = {"calc.exe"};
                 Process p = rt.exec(comando);
-
-                //¿Porque que hay que usar un manejador de procesos?
                 ProcessHandle handle = p.toHandle();
 
-                //Tambien se puede capturar el proceso asi
-                //ProcessHandle handle=ProcessHandle.current()
                 System.out.println("ID del proceso: " + handle.pid());
-                //Si hemos cojido al hijo porque el nombre del comando es C:\Program Files\Java\jdk-20\bin\java.exe?
                 System.out.println("Nombre del comando: " + handle.info().command().get());
                 System.out.println("Argumentos usados: " + Arrays.toString(handle.info().arguments().orElse(new String[0])));
                 System.out.println("Tiempo de comienzo: " + handle.info().startInstant().get());
                 System.out.println("Tiempo de CPU: " + handle.info().totalCpuDuration().get());
                 System.out.println("Propietario: " + handle.info().user().get());
-
-                //Para obtener el padre
-                //System.out.println("Padre"+ handle.parent().get().info().command().get());
-                //Numero de hijos 0? Porque la calculadora no es hijo de java
-                //Porque estamos cogiendo la info del proceso de la calculadora no de java
-                //Ahora magicamente tiene 2 hijos despues de 10 min
                 System.out.println("Número de hijos: " + handle.descendants().count());
+                
             } catch (IOException ex) {
                 Logger.getLogger(Main.class.getName()).log(Level.SEVERE, null, ex);
             }
