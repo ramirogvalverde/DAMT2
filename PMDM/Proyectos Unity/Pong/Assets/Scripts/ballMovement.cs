30253a602227b88c05b14cc49931cb3d784c0c50
ramirogvalverde <112170316+ramirogvalverde@users.noreply.github.com>
2023-09-26 21:05:22 +0200

    26/9

---
+++ PMDM/Proyectos Unity/Pong/Assets/Scripts/ballMovement.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ballMovement : MonoBehaviour
+{
+
+    [SerializeField]
+    private float speed = 4f;
+    [SerializeField]
+    private Rigidbody2D ballBody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ballBody = GetComponent<Rigidbody2D>();
+        launch();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void launch()
+    {
+        float xSpeed;
+        float ySpeed;
+
+        if(Random.Range(0,2)==0)
+        {
+            xSpeed = 1;
+        }
+        else
+        {
+            xSpeed = -1;
+        }
+
+        if (Random.Range(0, 2) == 0)
+        {
+            ySpeed = 1;
+        }
+        else
+        {
+            ySpeed = -1;
+        }
+
+        ballBody.velocity = new Vector2(xSpeed, ySpeed) * speed;
+
+    }
+}
---
+++ PMDM/Proyectos Unity/Pong/Assets/Scripts/paddlesMove.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paddlesMove : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private float speed = 7f;
+    [SerializeField]
+    private bool paddleid = false;
+
+    private float yLimit = 3.34f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float movement;
+
+        if(paddleid)
+        {
+            movement = Input.GetAxisRaw("Vertical");
+        }
+        else
+        {
+            movement = Input.GetAxisRaw("Vertical2");
+        }
+
+        //transform.position += new Vector3(0f, movement * speed * Time.deltaTime, 0f);
+
+        Vector2 paddlePostition = transform.position;
+        paddlePostition.y = Mathf.Clamp(paddlePostition.y + movement * speed *Time.deltaTime, -yLimit,yLimit);
+        transform.position = paddlePostition;
+
+        
+    }
+}
