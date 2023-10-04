bf8688b6bd2154c750f15cc1805fbb5c64709aa0
ramirogvalverde <112170316+ramirogvalverde@users.noreply.github.com>
2023-10-04 13:39:07 +0200

    PSP salida windows

--- PSP/EVALUACION_1/UD1_Programacion multiproceso/UD1_Hoja3_ comunicacion entre procesos/Ejercicio9/src/main/java/com/mycompany/ejercicio9/Ejercicio9.java
+++ PSP/EVALUACION_1/UD1_Programacion multiproceso/UD1_Hoja3_ comunicacion entre procesos/Ejercicio9/src/main/java/com/mycompany/ejercicio9/Ejercicio9.java
@@ -1,14 +1,11 @@
 package com.mycompany.ejercicio9;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
